85059d7f8d6735baaf376aabc342c17c9c397e90
David J. Arias López M <querquobad69@gmail.com>
2015-10-23 12:54:56 -0500

    Controladores de usuario y aspirante
    El controlador de usuario maneja las dos clases,

---
+++ controllers/aspirantes.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('../autoload.php');
+require_once('../sesiones.php');
+class Aspirantecontroller extends abstractDB {
+	protected $id;
+	private $rows;
+
+	public function get($request=array()) {
+		$arr_sql = array();
+		$sql = 'SELECT '.
+				'a.id, a.nombre, CONCAT_WS(\' \',a.paterno, a.materno) AS apellidos, a.fecha_nacimiento, if(a.sexo=true,\'1\',\'0\') as sexo, a.email, '.
+				'b.pais AS nacionalidad, c.pais AS lugar_nacimiento, d.religion as religion, e.estado_civil, f.tipo_casa, g.vivienda_compartida '.
+			'FROM '.
+				'aspirantes a '.
+			'LEFT JOIN paises b on a.nacionalidad = b.id '.
+			'LEFT JOIN paises c on a.lugar_nacimiento = c.id '.
+			'LEFT JOIN religiones d ON a.religion = d.id '.
+			'LEFT JOIN estados_civiles e ON a.estado_civil = e.id '.
+			'LEFT JOIN tipos_casas f ON a.tipo_casa = f.id '.
+			'LEFT JOIN cat_vivienda_compartida g ON a.vivienda_compartida_con = g.id';
+		if (isset($request['email'])) {
+			$sql .= ' WHERE a.email = ?';
+			$arr_sql[] = $request['email'];
+		}
+		$this->rows = $this->query($sql,$arr_sql);
+		if (!is_array($this->rows)) throw new RuntimeException('Error SQL');
+		return $this->rows;
+	}
+	public function set() {
+		return null;
+	}
+
+	public function edit() {
+		return null;
+	}
+
+	public function del() {
+		return null;
+	}
+}
+
+?>
---
+++ controllers/usuario.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once('../autoload.php');
+require_once('../sesiones.php');
+class Usuariocontroller extends abstractDB {
+	public $nombre;
+	public $apellido;
+	public $email;
+	private $password;
+	protected $id;
+	private $rows;
+
+	public function get($request=array()) {
+		$user_email = $request['email'];
+		if($user_email != '') {
+			$this->rows = $this->query('
+			SELECT '.
+				'id, email, password '.
+			'FROM '.
+				'usuarios '.
+			'WHERE '.
+				'email = ?',array($_POST['email']));
+			if (!is_array($this->rows) || count($this->rows) == 0) {
+				throw new RuntimeException('Usuario no encontrado');
+			}
+			//Tecnicamente el email es único por lo que sólo obtendremos un registro
+			return $this->rows;
+		} else {
+			throw new RuntimeException('E-Mail vacío');
+		}
+	}
+
+	public function set($user_data=array()) {
+		if(array_key_exists('email', $user_data)) {
+			$this->get($user_data['email']);
+			if($user_data['email'] != $this->email) {
+				foreach ($user_data as $campo=>$valor) {
+					$$campo = $valor;
+				}
+				$this->query_all('INSERT INTO usuarios (email, password) '.
+						'VALUES (?, PASSWORD(?))',array());
+				$this->mensaje = 'Usuario agregado exitosamente';
+			} else {
+				$this->mensaje = 'El usuario ya existe';
+			}
+		}
+	}
+
+	public function edit($user_data=array()) {
+		foreach ($user_data as $campo=>$valor) {
+			$$campo = $valor;
+		}
+		$this->query = 'UPDATE usuarios '.
+				'SET nombre=?, '.
+				'apellido=? '.
+				'WHERE email = ?';
+	}
+
+	public function del($user_email='') {
+		$this->query = 'DELETE FROM usuarios '.
+				'WHERE email = ?';
+	}
+
+	public function login ($args){
+		$retval =  $this->query('SELECT PASSWORD(?) = password as login FROM usuarios WHERE email = ?',array($args['password'],$args['email']));
+		if ($retval[0]['login'] == '1') $_SESSION['email'] = $_POST['email'];
+		return $retval;
+	}
+}
+require_once('aspirantes.php');
+$user = new Usuariocontroller();
+if (isset($_POST['request'])) {
+	try {
+		$datos = call_user_func(array($user,$_POST['request']),$_POST);
+	} catch (RuntimeException $e) {
+		$user = new Aspirantecontroller();
+		$datos = call_user_func(array($user,$_POST['request']),$_POST);
+	}
+	if (!is_array($datos)) throw new RuntimeException('No existe propiedad');
+} else {
+	throw new RuntimeException('request vacío');
+}
+$retval = json_encode(array('data' => $datos),JSON_FORCE_OBJECT);
+if (!$retval) {
+	error_log(json_last_error_msg());
+	foreach ($datos as &$dato_actual) foreach ($dato_actual as &$dato_real) $dato_real = utf8_encode($dato_real);
+	$retval = json_encode(array('data' => $datos));
+	if (!$retval) throw new RuntimeException('Error JSON:'.json_last_error_msg());
+}
+echo $retval;
+
+?>
