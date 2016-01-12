df468502e5c0a942f60f5171e80893045cc90309
David J. Arias López M <querquobad69@gmail.com>
2016-01-11 23:52:46 -0600

    No cargar sol_empleo.js asíncronamente
    sol_empleo.js tiene funciones necesarias para el document.ready por lo
    que se debe parsear antes de cargarse la página.

--- js/sol_empleo.js
+++ js/sol_empleo.js
@@ -1,37 +1,34 @@
-$(function() {
+usuario = {
+	email : ''
+}
 
-	usuario = {
-		email : ''
+function doAjax(obj,callback) {
+	var retval;
+	if (obj.url == undefined) {
+		obj.url = 'ws.php';
 	}
-
-	function doAjax(obj,callback) {
-		var retval;
-		if (obj.url == undefined) {
-			obj.url = 'ws.php';
-		}
-		if (obj.dataType == undefined || obj.jsonp == undefined) {
-			obj.dataType = json;
-			obj.jsonp = false;
-		}
-		if (obj.method == undefined && obj.type == undefined) { // en JQuery type y method son sinónimos
-			obj.method = 'post';
-		}
-		if (obj.xhrFields == undefined) {
-			obj.xhrFields = {withCredentials : true}
-		}
-		if (obj.data == undefined) {
-			console.log('Llamada AJAX sin datos!');
-			obj.data = {}
-		}
-		obj.success = function(data) {
-			retval = data
-		}
-		obj.error = function() {
-			retval = false;
-		}
-		obj.complete = function() {
-			callback(jqxhr,retval,text);
-		}
-		$.ajax(obj);
+	if (obj.dataType == undefined || obj.jsonp == undefined) {
+		obj.dataType = json;
+		obj.jsonp = false;
 	}
-});
+	if (obj.method == undefined && obj.type == undefined) { // en JQuery type y method son sinónimos
+		obj.method = 'post';
+	}
+	if (obj.xhrFields == undefined) {
+		obj.xhrFields = {withCredentials : true}
+	}
+	if (obj.data == undefined) {
+		console.log('Llamada AJAX sin datos!');
+		obj.data = {}
+	}
+	obj.success = function(data) {
+		retval = data
+	}
+	obj.error = function() {
+		retval = false;
+	}
+	obj.complete = function() {
+		callback(jqxhr,retval,text);
+	}
+	$.ajax(obj);
+}
