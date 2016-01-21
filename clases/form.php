66ce644afc36fa939a15a3145f67a4e37806d635
David J. Arias López M <querquobad69@gmail.com>
2016-01-21 17:36:22 -0600

    Ajustar clase formas horizontales e inline

--- clases/form.php
+++ clases/form.php
@@ -6,13 +6,11 @@ class form extends element {
 	public function __construct($att=array()) {
 		if (isset($att['tag']) && $att['tag'] !== 'form') throw new RuntimeException('No se puede crear un elemento '.$att['tag'].' con la clase form');
 		$att['tag'] = 'form';
-		if (in_array('role',$att)) {
-			if ($att['tipo'] === 'horizontal' || $att['tipo'] === 'inline') {
-				$this->tipo = $att['tipo'];
-				unset($att['tipo']);
-				$this->addAtributo('class','form-'.$att['tipo']);
-			}
+		if (isset($att['tipo']) && ($att['tipo'] === 'horizontal' || $att['tipo'] === 'inline')) {
+			$this->tipo = $att['tipo'];
+			$this->addAtributo('class','form-'.$att['tipo']);
 		}
+		if(isset($att['tipo'])) unset($att['tipo']);
 		$this->addAtributo('role','form');
 		parent::__construct($att);
 	}
